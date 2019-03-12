3d94d6717abce1f4ed51ae55e73c90bffa3c8dbe
jxu023 <jxu023@ucr.edu>
2019-03-11 21:39:30 -0700

    using Array instead of lists now, starting on game loop

--- tic-tac-toe.hs
+++ tic-tac-toe.hs
@@ -1,39 +1,9 @@
 
 import Data.Array
 
-zipPlus coord diff = zipWith (+) coord diff
-
 type Coord = (Int, Int)
 
-get :: Board -> Coord -> Char
-get b c = (!)
-
-endp :: Board -> Coord -> Bool
-endp board coord 
-    = moved && (winner || tie)
-      where moved = xo /= '_'
-            winner = any (== 2) $ map (\dir -> sum $ map count [map (* (-1)) dir, dir])
-                                      [[0, 1], [1, 1], [1, 0], [1, -1]]
-            count diff = follow $ next coord
-                where follow crd = if valid crd && get board crd == xo
-                                      then (+ 1) . follow $ next crd
-                                      else 0
-                      next x = zipPlus x diff
-            valid coord = and [pred dim | dim <- coord, pred <- [(>= 0), (< 3)]]
-            tie = (not . any (== '_') $ concat board)
-            xo = get board coord
-
-data Result = X_Win | O_win | Tie
-
--- this to monads .. doesn't have to be explicit really though
-data Game = InPlay | Result
-
-newtype Board = Board (Array (Integer, Integer) Char)
-
-showBoard :: Board -> String
-showBoard (Board b) =
-    unlines (map (\r -> concat $ map (\c -> [' ', b ! (r, c)]) [sc..ec]) [sr..er])
-    where ((sr, sc), (er, ec)) = bounds b
+newtype Board = Board (Array (Int, Int) Char)
 
 instance Show Board where
     show = showBoard
@@ -41,9 +11,39 @@ instance Show Board where
 allCells :: (Num a, Enum a) => [(a, a)]
 allCells = [(i, j) | i <- [0..2], j <- [0..2]]
 
+showBoard :: Board -> String
+showBoard (Board b) =
+    unlines (map (\r -> concat $ map (\c -> [' ', b ! (r, c)]) [sc..ec]) [sr..er])
+    where ((sr, sc), (er, ec)) = bounds b
+
+endp :: Board -> Coord -> Bool
+endp (Board b) coord 
+    = moved && (winner || tie)
+      where moved = xo /= '_'
+            winner = any (== 2) $ map (\dir@(x, y) -> sum $ map count
+                                                                [((-1)*x, (-1)*y), dir])
+                                      [(0, 1), (1, 1), (1, 0), (1, -1)]
+            count diff = follow $ next coord
+                where follow crd = if valid crd && b ! crd == xo
+                                      then 1 + (follow (next crd))
+                                      else 0
+                      next (x, y) = (x + fst diff, y + snd diff)
+            valid (x, y) = val x && val y where val x = x >= 0 && x < 3
+            tie = all (== '_') [b ! cell | cell <- allCells]
+            xo = b ! coord
+
 emptyBoard :: Board
 emptyBoard = Board $ array ((0,0), (2,2)) [(coord, '_') | coord <- allCells]
 
+data Result = X_Win | O_Win | Tie
+data Turn = X_Turn | O_Turn
+data InPlay = InPlay
+    { board :: Board
+    , turn :: Turn
+    }
+data Game = InPlay | Result
+
 main = do 
-    print $ endp ["XOX", "OXX", "O_X"] [0, 0]
+    -- print $ endp ["XOX", "OXX", "O_X"] [0, 0]
     print emptyBoard
+    print $ endp emptyBoard (0,0)
